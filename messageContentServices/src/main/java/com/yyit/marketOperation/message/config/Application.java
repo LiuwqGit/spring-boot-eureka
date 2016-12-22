04831d6c4184c1c3cdc81aa6ad80cc1a687a5b0d
liuweiqin <772863869@qq.com>
2016-12-22 21:33:34 +0800

    @ComponentScan(basePackages={com.yyit.*})  add scan component package

--- messageContentServices/src/main/java/com/yyit/marketOperation/message/config/Application.java
+++ messageContentServices/src/main/java/com/yyit/marketOperation/message/config/Application.java
@@ -3,8 +3,10 @@ package com.yyit.marketOperation.message.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
+@ComponentScan(basePackages={"com.yyit.*"})
 @SpringBootApplication
 public class Application {
 
