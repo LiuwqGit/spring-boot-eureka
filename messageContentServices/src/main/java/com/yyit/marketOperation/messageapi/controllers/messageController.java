94825710e45905ffa972e43afb6242412e98e207
liuweiqin <772863869@qq.com>
2016-12-22 20:55:10 +0800

    add eureka-server

--- messageContentServices/src/main/java/com/yyit/marketOperation/messageapi/controllers/messageController.java
+++ messageContentServices/src/main/java/com/yyit/marketOperation/messageapi/controllers/messageController.java
@@ -1,5 +1,13 @@
 package com.yyit.marketOperation.messageapi.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class messageController {
 
+	@RequestMapping("/test")
+	public String test() {
+		return "啦啦啦，成功rest api";
+	}
 }
