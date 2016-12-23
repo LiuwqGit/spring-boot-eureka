4f570835d82274bf64250bb62c1e21b65b3684f3
liuweiqin <772863869@qq.com>
2016-12-23 08:58:14 +0800

    add sendmessage

--- messageContentServices/src/main/java/com/yyit/marketOperation/messageapi/controllers/messageController.java
+++ messageContentServices/src/main/java/com/yyit/marketOperation/messageapi/controllers/messageController.java
@@ -1,5 +1,8 @@
 package com.yyit.marketOperation.messageapi.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,14 @@ public class messageController {
 	@RequestMapping("/test")
 	public String test() {
 		return "啦啦啦，成功rest api";
+	}
+
+	@RequestMapping("/sendmessage")
+	public Object sendmessage() {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		
+		return map;
+
 	}
 }
